f94f1d6aa6d79aeec76e0c13de6016a4f55c48f3
Muskaan jain <muskaanjain017@gmail.com>
2023-08-04 08:54:17 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const[result,setResult] = useState("");
+
+  const handleClick =(e) =>{
+    setResult(result.concat(e.target.name));
+  }
+  const calculate = () =>{
+    try{
+      setResult(eval(result).toString());
+    }
+    catch(err){
+      setResult("Error");
+    }
+    
+  }
+
+  document.onkeydown = k =>{
+    if(k.key==='Backspace') {
+      setResult(result.slice(0,result.length-1))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Calculator">
+          <form>
+            <input type="text" placeholder='0' value={result}/>
+          </form>
+          <div className="gridContainer">
+            <button onClick={handleClick} name="7">7</button>
+            <button onClick={handleClick} name="8">8</button>
+            <button onClick={handleClick} name="9">9</button>
+            <button onClick={handleClick} name="/">&divide;</button>
+            <button onClick={handleClick} name="4">4</button>
+            <button onClick={handleClick} name="5">5</button>
+            <button onClick={handleClick} name="6">6</button>
+            <button onClick={handleClick} name="*">&times;</button>
+            <button onClick={handleClick} name="1">1</button>
+            <button onClick={handleClick} name="2">2</button>
+            <button onClick={handleClick} name="3">3</button>
+            <button onClick={handleClick} name="-">&ndash;</button>
+            <button onClick={handleClick} name="0">0</button>
+            <button onClick={handleClick} name=".">.</button>
+            <button onClick={handleClick} name="+">+</button>
+            <button onClick={calculate} id="equals" name="=">=</button>
+          </div>
+      </div>
+    </>
   );
 }
 
